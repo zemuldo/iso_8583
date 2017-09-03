351582a40c463a818db3bbff7cf2813499294af7
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-03 19:44:11 +0300

    updates test cases

--- test/testMainBmp.js
+++ test/testMainBmp.js
@@ -45,4 +45,6 @@ console.log(iso.getIsoJSON(iso.getBufferMessage()))
 console.log(iso1.getIsoJSON(iso.getBufferMessage()))
 console.log(iso.attachTimeStamp())
 console.log(iso1.attachTimeStamp())
+console.log(iso.getTType())
+console.log(iso.getAccType())
 
