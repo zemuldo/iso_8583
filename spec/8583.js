2eec21717e48b8d63f416f6ad1988039fb0c5282
Tim Gabets <tim@gabets.ru>
2017-11-23 22:24:18 +0300

    getTType() test cases

--- spec/8583.js
+++ spec/8583.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import Main from '../lib/8583.js';
 
+/*
 let data = {
    0: "0100",
    2: "4761739001010119",
@@ -28,7 +29,19 @@ let data = {
    127: "000000800000000001927E1E5F7C0000000000000000500000000000000014A00000000310105C000128FF0061F379D43D5AEEBC8002800000000000000001E0302031F000203001406010A03A09000008CE0D0C840421028004880040417091180000014760BAC24959"
 };
 
+let isopack = new Main(data);
 test('should validate message', t => {
-  let isopack = new Main(data);
   t.is(isopack.validateMessage(), true);
 });
+*/
+
+/**
+ * getTType() test cases
+ */
+test('should return proper transaction type description fro type 00', t => {
+   let data = { 3: "000000" };
+   let isopack = new Main(data);
+
+   t.is(isopack.getTType(), 'Goods and services');
+});
+
