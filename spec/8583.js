68e47d9c6dce531c88354eed5e54f1813dda721e
Tim Gabets <tim@gabets.ru>
2017-11-24 18:58:29 +0300

    Added dummy valideMessage() test case

--- spec/8583.js
+++ spec/8583.js
@@ -107,5 +107,40 @@ test('getAccountTypeTo() should return proper 30 account type', t => {
   t.is(isopack.getAccountTypeTo(), 'Credit account');
 });
 
+/**
+ * validateMessage() test cases
+ */
+test('validateMessage() should validate generic message from README', t => {
+  let data = {
+    0: '0100',
+    2: '4761739001010119',
+    3: '000000',
+    4: '000000005000',
+    7: '0911131411',
+    12: '131411',
+    13: '0911',
+    14: '2212',
+    18: '4111',
+    22: '051',
+    23: '001',
+    25: '00',
+    26: '12',
+    32: '423935',
+    33: '111111111',
+    35: '4761739001010119D22122011758928889',
+    41: '12345678',
+    42: 'MOTITILL_000001',
+    43: 'My Termianl Business                    ',
+    49: '404',
+    52: '7434F67813BAE545',
+    56: '1510',
+    123: '91010151134C101',
+    127: '000000800000000001927E1E5F7C0000000000000000500000000000000014A00000000310105C000128FF0061F379D43D5AEEBC8002800000000000000001E0302031F000203001406010A03A09000008CE0D0C840421028004880040417091180000014760BAC24959'
+  };
+
+  let isopack = new Main(data);
+  t.is(isopack.validateMessage(), true);
+});
+
 
 
