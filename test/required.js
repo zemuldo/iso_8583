a4c3029aa3dbd4430ccd8ea4db2d4d05ab11fc1f
Lucas Inocente <olucassanchez@gmail.com>
2018-08-03 17:04:43 -0300

    Refactory required fields to run with main validate function

--- test/required.js
+++ test/required.js
@@ -17,17 +17,6 @@ test('should return error because required is not implemented on processing code
   t.deepEqual(required(data), 'required is not implemented on processing code: 000001');
 });
 
-test('should return error because fields [0, 1, 2, 4] is required but dont have 1 in the data', t => {
-  let data = {
-    0: 'a',
-    2: 'b',
-    3: '000000',
-    4: 'c'
-  };
-
-  t.deepEqual(required(data), {error: 'Processing code: 000000 - Missing required fields: 1'});
-});
-
 test('should return error because fields [0, 1, 2, 4] is required but dont have 1 and 4 in the data', t => {
   let data = {
     0: 'a',
@@ -35,7 +24,7 @@ test('should return error because fields [0, 1, 2, 4] is required but dont have 
     3: '000000'
   };
 
-  t.deepEqual(required(data), {error: 'Processing code: 000000 - Missing required fields: 1,4'});
+  t.deepEqual(required(data), {error: 'Processing code: 000000 - Missing required fields: 4'});
 });
 
 test('should return error because fields [0] is required', t => {
@@ -46,4 +35,15 @@ test('should return error because fields [0] is required', t => {
   };
 
   t.deepEqual(required(data), {error: 'Processing code: 000009 - Missing required fields: 0'});
+});
+
+test('should return true because fields [0, 2, 4] is required', t => {
+  let data = {
+    0: 'a',
+    2: 'b',
+    3: '000000',
+    4: 'c'
+  };
+
+  t.is(required(data), true);
 });
