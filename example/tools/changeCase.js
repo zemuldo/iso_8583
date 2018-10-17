a9467581bac56a3ffd56222577666ac5b477837a
Danstan Onyangoe ~ Zemuldo <danstan.otieno@gmail.com>
2018-10-17 16:01:35 +0300

    clean example

--- example/tools/changeCase.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  firstCaps: string => string.charAt(0).toUpperCase() + string.slice(1),
-};
--- example/tools/getRetransmitMti.js
+++
@@ -1,7 +0,0 @@
-module.exports = (originlMti) => {
-  const newMti = originlMti.split('');
-  const lastDigit = parseInt(newMti[3], 10);
-  const newLastDigit = lastDigit + 1;
-  newMti[3] = newLastDigit;
-  return newMti.join('');
-};
